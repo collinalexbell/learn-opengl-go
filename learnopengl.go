36c09b4c9b72484cf5a1e9c7a7083929fa50073b
kuberlog <collinalexbell@gmail.com>
2021-08-10 22:01:38 -0500

    clear to cyberpunk color

--- learnopengl.go
+++ learnopengl.go
@@ -1,11 +1,20 @@
 package main // import "github.com/go-gl/example/gl41core-cube"
 
 import (
-	"fmt"
-
 	"github.com/go-gl/gl/v4.6-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
+
+func clearToCyberpunkColor() {
+	gl.ClearColor(4.0/255.0, 217.0/255.0, 255.0/255.0, 255.0/255.0)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+}
+
+func processInput(window *glfw.Window) {
+	if(window.GetKey(glfw.KeyEscape) == glfw.Press) {
+		window.SetShouldClose(true)
+	}
+}
 
 func main() {
 	err := glfw.Init()
@@ -29,12 +38,13 @@ func main() {
 	gl.Viewport(0, 0, 800, 600)
 
 	window.SetFramebufferSizeCallback(func(w *glfw.Window, width int, height int) {
-		fmt.Println("resize")
 		gl.Viewport(0, 0, int32(width), int32(height))
 	})
 
 	for !window.ShouldClose() {
 		// Do OpenGL stuff.
+		processInput(window)
+		clearToCyberpunkColor()
 		window.SwapBuffers()
 		glfw.PollEvents()
 	}
